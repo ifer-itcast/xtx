af7447d95ca32e465a52e4f17191d1feb600fd40
ifer-itcast <yangkangkang@itcast.cn>
2021-06-24 19:44:32 +0800

    基本使用Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-  <div id="nav">App {{ $store.state.username }}</div>
+  <div id="nav">
+    <p>{{ $store.state.username }}</p>
+    <p>{{ $store.getters.newName }}</p>
+    <button @click="handleClick1">update username</button>
+    <button @click="handleClick2">update username async</button>
+  </div>
 </template>
 <script>
 import { useStore } from 'vuex'
 export default {
   name: 'App',
   setup() {
+    // 0
     const store = useStore()
+    // 1
     console.log(store.state.username)
+    // 2
+    console.log(store.getters.newName)
+    // 3
+    const handleClick1 = () => {
+      store.commit('updateName')
+    }
+    const handleClick2 = () => {
+      store.dispatch('updateName')
+    }
+    return {
+      handleClick1,
+      handleClick2
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,21 @@ export default createStore({
     username: 'ifer'
   },
   mutations: {
+    updateName(state) {
+      state.username = 'elser'
+    }
   },
   actions: {
+    updateName(ctx) {
+      setTimeout(() => {
+        ctx.commit('updateName')
+      }, 1000)
+    }
   },
-  modules: {
+  modules: {},
+  getters: {
+    newName(state) {
+      return state.username + '~~'
+    }
   }
 })
