afc8b951bf41bb381f5ae38136c9cde53eb3f1cc
ifer-itcast <yangkangkang@itcast.cn>
2021-06-26 21:59:54 +0800

    测试请求工具函数

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="nav">
     <p>App</p>
-    <p>{{ $store.state.user.profile.account }}</p>
-    <button @click="$store.commit('user/setUser', { account: 'xxx' })">
-      click
-    </button>
+    <button @click="handleClick">click</button>
   </div>
 </template>
 <script>
+import request from '@/utils/request'
 export default {
   name: 'App',
-  setup() {}
+  setup() {
+    const handleClick = () => {
+      request('/member/profile', 'get', {
+        a: 10
+      })
+      /* request('/member/profile', 'post', {
+        a: 10
+      }) */
+      // 输入以下地址，然后点击按钮，测试响应拦截器
+      // http://localhost:8080/#/abc?age=888
+    }
+    return { handleClick }
+  }
 }
 </script>
