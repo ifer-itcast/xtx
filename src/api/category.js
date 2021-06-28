26b7cf3fa1373c0077196651f27b957f28a51bc6
ifer-itcast <yangkangkang@itcast.cn>
2021-06-28 10:04:06 +0800

    获取头部分类导航数据并渲染

---
+++ src/api/category.js
@@ -0,0 +1,10 @@
+// 分类相关的 API 接口
+import request from '@/utils/request'
+
+/**
+ * 获取所有分类（顶级、二级、对应的商品推荐数据）
+ * @returns Promise
+ */
+export const findAllCategory = () => {
+  return request('/home/category/head', 'get')
+}
--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -1,9 +1,24 @@
+import { findAllCategory } from '@/api/category'
+import { topCategory } from '@/api/constants'
 export default {
   namespaced: true,
   state() {
     return {
-      // 分类信息
-      list: []
+      // 默认数据，防止接口请求慢导致白屏
+      list: topCategory.map(item => ({ name: item }))
+    }
+  },
+  mutations: {
+    setList(state, payload) {
+      state.list = payload
+    }
+  },
+  actions: {
+    async getList({ commit }) {
+      // 获取分类数据
+      const data = await findAllCategory()
+      // 修改分类数据
+      commit('setList', data.result)
     }
   }
 }
