13e1f61d3c1be220eea91834fbd62d83de65e4b5
ifer-itcast <yangkangkang@itcast.cn>
2021-06-28 13:03:58 +0800

    首页主体左侧分类结构渲染

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="container">
-    Home
+    <!-- 首页入口（分类+广告） -->
+    <div class="home-entry">
+      <!-- 左侧分类 -->
+      <HomeCategory />
+      <!-- 轮播图 -->
+    </div>
   </div>
 </template>
 
 <script>
+import HomeCategory from './components/home-category.vue'
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    HomeCategory
+  }
 }
 </script>
 
-<style lang="less" scoped>
-.container {
-  height: 200px;
-  .hoverShadow();
-}
-</style>
+<style lang="less" scoped></style>
