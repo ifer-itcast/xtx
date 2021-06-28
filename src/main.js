a5bbe3fc5b0d50d50a3ea1323c80c939e233b43e
ifer-itcast <yangkangkang@itcast.cn>
2021-06-28 08:39:52 +0800

    引入第三方重置样式和自己的公用样式

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// #1 第三方的重置样式
+import 'normalize.css'
+// #2 自己的公用样式
+import '@/assets/styles/common.less'
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app')
