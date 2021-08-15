41502e2d18b79f5b6f91d04433f89782771c5fe4
ifer-itcast <yangkangkang@itcast.cn>
2021-08-15 15:38:45 +0800

    .

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,9 +1,12 @@
 import defaultImg from '@/assets/images/200.png'
-import XtxSkeleton from './xtx-skeleton.vue'
+/* import XtxSkeleton from './xtx-skeleton.vue'
 import XtxCarousel from './xtx-carousel.vue'
 import XtxMore from './xtx-more.vue'
 import XtxBread from './xtx-bread.vue'
-import XtxBreadItem from './xtx-bread-item.vue'
+import XtxBreadItem from './xtx-bread-item.vue' */
+
+// 目录、是否加载子目录、匹配规则
+const importFn = require.context('./', false, /\.vue$/)
 
 /**
  * 图片懒加载
@@ -38,11 +41,15 @@ export default {
   install(app) {
     // Vue3 在 app 上进行扩展，app 提供 component、directive 等函数
     // Vue3 中可以通过 app.config.globalProperties 把属性或方法挂载到 Vue 原型上（Vue.prototype）
-    app.component(XtxSkeleton.name, XtxSkeleton)
+    /* app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
     app.component(XtxMore.name, XtxMore)
     app.component(XtxBread.name, XtxBread)
-    app.component(XtxBreadItem.name, XtxBreadItem)
+    app.component(XtxBreadItem.name, XtxBreadItem) */
+    importFn.keys().forEach(path => {
+      const component = importFn(path).default
+      app.component(component.name, component)
+    })
     // 定义指令
     defineDirective(app)
   }
