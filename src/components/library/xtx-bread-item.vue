895b6314d2407cfe8cdc46a8cc6e50ecde90d012
ifer-itcast <yangkangkang@itcast.cn>
2021-08-15 15:27:34 +0800

    .

--- src/components/library/xtx-bread-item.vue
+++ src/components/library/xtx-bread-item.vue
@@ -3,7 +3,7 @@
     <RouterLink v-if="to" :to="to"><slot /></RouterLink>
     <span v-else><slot /></span>
   </div>
-  <i class="iconfont icon-angle-right"></i>
+  <!-- <i class="iconfont icon-angle-right"></i> -->
 </template>
 <script>
 export default {
--- src/components/library/xtx-bread.vue
+++ src/components/library/xtx-bread.vue
@@ -1,12 +1,34 @@
-<template>
-  <div class="xtx-bread">
-    <slot />
-  </div>
-</template>
-
 <script>
+import { h } from 'vue'
 export default {
-  name: 'XtxBread'
+  name: 'XtxBread',
+  render() {
+    // 标签名、属性对象、子节点
+    // 1. 创建 xtx-bread 父容器
+    // 2. 获取默认插槽内容
+    // 3. 去除 xtx-break-item 组件的 i 标签，因为由 render 函数来组织
+    // 4. 遍历插槽中的 item，得到一个动态创建的节点，最后一个 item 不加 i 标签
+    // 5. 把动态创建的节点渲染到 xtx-bread 容器中
+    const items = this.$slots.default()
+    const dymanicItems = []
+    items.forEach((item, i) => {
+      dymanicItems.push(item)
+      if (i < items.length - 1) {
+        dymanicItems.push(
+          h('i', {
+            class: 'iconfont icon-angle-right'
+          })
+        )
+      }
+    })
+    return h(
+      'div',
+      {
+        class: 'xtx-bread'
+      },
+      dymanicItems
+    )
+  }
 }
 </script>
 
@@ -28,9 +50,6 @@ export default {
     margin-left: 5px;
     margin-right: 5px;
     line-height: 22px;
-    &:last-child {
-      display: none;
-    }
   }
 }
 </style>
