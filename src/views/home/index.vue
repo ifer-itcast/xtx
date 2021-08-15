81dc09657b92d3c59e28b19f4fbce49440046118
ifer-itcast <yangkangkang@itcast.cn>
2021-08-15 09:27:44 +0800

    .

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -10,18 +10,22 @@
   </div>
   <!-- 新鲜好物 -->
   <home-new/>
+  <!-- 人气推荐 -->
+  <home-hot/>
 </template>
 
 <script>
 import HomeCategory from './components/home-category.vue'
 import HomeBanner from './components/home-banner.vue'
 import HomeNew from './components/home-new.vue'
+import HomeHot from './components/home-hot.vue'
 export default {
   name: 'HomePage',
   components: {
     HomeCategory,
     HomeBanner,
-    HomeNew
+    HomeNew,
+    HomeHot
   }
 }
 </script>
