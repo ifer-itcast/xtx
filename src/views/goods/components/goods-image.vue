1e1566ea50da5f6578cdcf73abd24880c46733ea
ifer-itcast <yangkangkang@itcast.cn>
2021-08-18 09:12:15 +0800

    .

--- src/views/goods/components/goods-image.vue
+++ src/views/goods/components/goods-image.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="goods-image">
-    <div class="middle">
+    <!-- 大图 -->
+    <div v-show="show" class="large" :style="[{ backgroundImage: `url(${images[currIndex]})` }, largePosition]"></div>
+    <!-- 中图 -->
+    <div class="middle" ref="target">
       <img :src="images[currIndex]" alt="" />
+      <!-- 遮罩 -->
+      <div v-show="show" class="layer" :style="layerPosition"></div>
     </div>
+    <!-- 小图 -->
     <ul class="small">
       <li v-for="(img, i) in images" :key="img" :class="{ active: i === currIndex }">
         <img @mouseenter="currIndex = i" :src="img" alt="" />
@@ -11,7 +17,8 @@
   </div>
 </template>
 <script>
-import { ref } from 'vue'
+import { ref, reactive, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 export default {
   name: 'GoodsImage',
   props: {
@@ -22,7 +29,47 @@ export default {
   },
   setup() {
     const currIndex = ref(0)
-    return { currIndex }
+    // 是否显示遮罩和大图
+    const show = ref(false)
+    // 遮罩
+    const layerPosition = reactive({
+      left: 0,
+      top: 0
+    })
+    // 大图
+    const largePosition = reactive({
+      backgounndPositionX: 0,
+      backgroundPositionY: 0
+    })
+    const target = ref(null)
+    const { elementX, elementY, isOutside } = useMouseInElement(target)
+    watch([elementX, elementY, isOutside], () => {
+      show.value = !isOutside.value
+      const position = {
+        x: 0,
+        y: 0
+      }
+      if (elementX.value < 100) {
+        position.x = 0
+      } else if (elementX.value > 300) {
+        position.x = 200
+      } else {
+        position.x = elementX.value - 100
+      }
+      if (elementY.value < 100) {
+        position.y = 0
+      } else if (elementY.value > 300) {
+        position.y = 200
+      } else {
+        position.y = elementY.value - 100
+      }
+      // 给样式赋值
+      layerPosition.left = position.x + 'px'
+      layerPosition.top = position.y + 'px'
+      largePosition.backgroundPositionX = -2 * position.x + 'px'
+      largePosition.backgroundPositionY = -2 * position.y + 'px'
+    })
+    return { currIndex, show, layerPosition, largePosition, target }
   }
 }
 </script>
@@ -32,10 +79,32 @@ export default {
   height: 400px;
   position: relative;
   display: flex;
+  z-index: 500;
+  .large {
+    position: absolute;
+    top: 0;
+    left: 412px;
+    width: 400px;
+    height: 400px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-repeat: no-repeat;
+    background-size: 800px 800px;
+    background-color: #f8f8f8;
+  }
   .middle {
     width: 400px;
     height: 400px;
     background: #f5f5f5;
+    position: relative;
+    cursor: move;
+    .layer {
+      width: 200px;
+      height: 200px;
+      background: rgba(0, 0, 0, 0.2);
+      left: 0;
+      top: 0;
+      position: absolute;
+    }
   }
   .small {
     width: 80px;
