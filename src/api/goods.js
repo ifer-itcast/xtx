6dc07535aa92957835586777b0d376926b1a1e14
ifer-itcast <yangkangkang@itcast.cn>
2021-08-19 11:03:54 +0800

    .

--- src/api/goods.js
+++ src/api/goods.js
@@ -2,6 +2,9 @@ import request from '@/utils/request'
 
 /**
  * 获取热榜商品
+ * @param {String} id 商品 ID
+ * @param {Integer} type 类型
+ * @param {Integer} limit 数量
  */
 export const findHotGoods = ({ id, type, limit = 3 }) => {
   return request('/goods/hot', 'get', { id, type, limit })
