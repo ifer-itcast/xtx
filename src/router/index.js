45d9fea885a9b8aabf2f38b277ce3d52fac99a29
ifer-itcast <yangkangkang@itcast.cn>
2021-08-21 10:27:36 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ const Home = () => import('@/views/home')
 const TopCategory = () => import('@/views/category/index')
 const SubCategory = () => import('@/views/category/sub')
 const Goods = () => import('@/views/goods')
+const Login = () => import('@/views/login/index')
 
 const routes = [
   // 一级路由布局容器
@@ -11,21 +12,13 @@ const routes = [
     path: '/',
     component: Layout,
     children: [
-      {
-        path: '/',
-        component: Home
-      },
-      {
-        path: '/category/:id',
-        component: TopCategory
-      },
-      {
-        path: '/category/sub/:id',
-        component: SubCategory
-      },
+      { path: '/', component: Home },
+      { path: '/category/:id', component: TopCategory },
+      { path: '/category/sub/:id', component: SubCategory },
       { path: '/product/:id', component: Goods }
     ]
-  }
+  },
+  { path: '/login', component: Login }
 ]
 
 const router = createRouter({
