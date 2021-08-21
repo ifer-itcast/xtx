3686eadc6f4c99ba01a378198f401fb20d2c316d
ifer-itcast <yangkangkang@itcast.cn>
2021-08-21 09:35:04 +0800

    .

--- src/components/library/xtx-pagination.vue
+++ src/components/library/xtx-pagination.vue
@@ -12,8 +12,47 @@
   </div>
 </template>
 <script>
+import { ref, computed } from 'vue'
 export default {
-  name: 'XtxPagination'
+  name: 'XtxPagination',
+  setup() {
+    // 总条数
+    const total = ref(100)
+    // 每页条数
+    const pagesize = ref(10)
+    // 当前第几页
+    const pagenum = ref(1)
+    // 按钮个数
+    const btnCount = 5
+
+    // 重点：根据上述数据得到（总页数，起始页码，结束页码，按钮数组）
+    const pager = computed(() => {
+      // 总页数
+      const pageCount = Math.ceil(total.value / pagesize.value)
+      // 计算起始页码和结束页码
+      // 1. 理想情况根据当前页码，和按钮个数可得到
+      let start = pagenum.value - Math.floor(btnCount / 2)
+      let end = start + btnCount - 1
+      // 2.1 如果起始页码小于1了，需要重新计算
+      if (start < 1) {
+        start = 1
+        end = start + btnCount - 1 > pageCount ? pageCount : start + btnCount - 1
+      }
+      // 2.2 如果结束页码大于总页数，需要重新计算
+      if (end > pageCount) {
+        end = pageCount
+        start = end - btnCount + 1 < 1 ? 1 : end - btnCount + 1
+      }
+      // 处理完毕start和end得到按钮数组
+      const btnArr = []
+      for (let i = start; i <= end; i++) {
+        btnArr.push(i)
+      }
+      return { pageCount, start, end, btnArr }
+    })
+
+    return { pager, pagenum }
+  }
 }
 </script>
 <style scoped lang="less">
