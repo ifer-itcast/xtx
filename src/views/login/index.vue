ab8ace8e607d77865b825cc733edd6a1ee381309
ifer-itcast <yangkangkang@itcast.cn>
2021-08-21 10:38:40 +0800

    .

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,15 +3,23 @@
   <section class="login-section">
     <div class="wrapper">
       <nav>
-        <a href="javascript:;">账户登录</a>
-        <a href="javascript:;">扫码登录</a>
+        <a @click="activeName = 'account'" :class="{ active: activeName === 'account' }" href="javascript:;">账户登录</a>
+        <a @click="activeName = 'qrcode'" :class="{ active: activeName === 'qrcode' }" href="javascript:;">扫码登录</a>
       </nav>
+      <!-- 表单 -->
+      <div v-if="activeName === 'account'" class="account-box">表单</div>
+      <!-- 二维码 -->
+      <div v-if="activeName === 'qrcode'" class="qrcode-box">
+        <img src="@/assets/images/qrcode.jpg" alt="" />
+        <p>打开 <a href="javascript:;">小兔鲜App</a> 扫码登录</p>
+      </div>
     </div>
   </section>
   <LoginFooter />
 </template>
 
 <script>
+import { ref } from 'vue'
 import LoginHeader from './components/login-header'
 import LoginFooter from './components/login-footer'
 export default {
@@ -19,6 +27,10 @@ export default {
   components: {
     LoginHeader,
     LoginFooter
+  },
+  setup() {
+    const activeName = ref('account')
+    return { activeName }
   }
 }
 </script>
@@ -59,6 +71,18 @@ export default {
           font-weight: bold;
         }
       }
+    }
+  }
+}
+
+.qrcode-box {
+  text-align: center;
+  padding-top: 40px;
+  p {
+    margin-top: 20px;
+    a {
+      color: @xtxColor;
+      font-size: 16px;
     }
   }
 }
